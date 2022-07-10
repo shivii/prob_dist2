7208b85ff7ebe11755e27af72b8fd2709a9d6f42
Shivangi Nigam <shivi.nigam15@gmail.com>
2022-07-11 04:16:14 +0530

    add net_G_B

--- models/base_model.py
+++ models/base_model.py
@@ -181,7 +181,7 @@ class BaseModel(ABC):
         """
         for name in self.model_names:
             if isinstance(name, str):
-                load_filename = '%s_net_%s_A.pth' % (epoch, name)
+                load_filename = '%s_net_%s_B.pth' % (epoch, name)
                 load_path = os.path.join(self.save_dir, load_filename)
                 net = getattr(self, 'net' + name)
                 if isinstance(net, torch.nn.DataParallel):
