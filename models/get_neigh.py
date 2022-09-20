0c1a74320a61c52e46dc4de60a51c9901b75f14d
Shivangi Nigam <shivi.nigam15@gmail.com>
2022-09-20 13:39:09 +0530

    to device : added in get neigh all vectors

--- models/get_neigh.py
+++ models/get_neigh.py
@@ -8,9 +8,11 @@ def get_neighb_list(image, label):
     image = torch.squeeze(image)
     print("image:", image.shape)
     shape_i, shape_j, shape_k  = image.shape
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     
-    features = torch.zeros((0,6), dtype=torch.float32)
-    labels = torch.zeros((0,1), dtype=torch.float32)
+    
+    features = torch.zeros((0,6), dtype=torch.float32).to(device)
+    labels = torch.zeros((0,1), dtype=torch.float32).to(device)
     for i, x in enumerate(image):
         #print("x1", x.shape)
         #print("----------------------computing neighbours")
@@ -18,7 +20,7 @@ def get_neighb_list(image, label):
             #print("x2", x.shape)
             for k, x in enumerate(x):
                 #print("-->>---x3-------------------------------------------------------", x)
-                feature = torch.zeros((0), dtype=torch.float32)
+                feature = torch.zeros((0), dtype=torch.float32).to(device)
                 fr = torch.zeros((1), dtype=torch.float32)
                 up = torch.zeros((1), dtype=torch.float32)
                 lf = torch.zeros((1), dtype=torch.float32)
@@ -27,7 +29,7 @@ def get_neighb_list(image, label):
                 bc = torch.zeros((1), dtype=torch.float32)
                 
                 if i == 0:
-                  fr = fr
+                  fr = fr.to(device)
                   #print("shape fr:", fr, fr.shape)
                 else:
                   fr = torch.unsqueeze(image[i-1,j,k], 0)
@@ -36,7 +38,7 @@ def get_neighb_list(image, label):
                 feature = torch.cat((feature, fr), 0)
                 
                 if j == 0:
-                  up = up
+                  up = up.to(device)
                   #print("shape up:", up , up.shape)
                 else:
                   up = torch.unsqueeze(image[i,j-1,k], 0)
@@ -45,7 +47,7 @@ def get_neighb_list(image, label):
                 feature = torch.cat((feature, up), 0)
                 
                 if k == 0:
-                  lf = lf
+                  lf = lf.to(device)
                   #print("shape lf:", lf, lf.shape)
                 else:
                   lf = torch.unsqueeze(image[i,j,k-1], 0)
@@ -54,7 +56,7 @@ def get_neighb_list(image, label):
                 feature = torch.cat((feature, lf), 0)
                 
                 if k+1 > shape_j-1:
-                  rt = rt
+                  rt = rt.to(device)
                   #print("shape rt:", rt, rt.shape)
                 else:
                   rt = torch.unsqueeze(image[i,j,k+1], 0)
@@ -63,7 +65,7 @@ def get_neighb_list(image, label):
                 feature = torch.cat((feature, rt), 0)
                 
                 if j+1 > shape_j-1:
-                  dw = dw
+                  dw = dw.to(device)
                   #print("shape dw:", dw, dw.shape)
                 else:
                   dw = torch.unsqueeze(image[i,j+1,k], 0)
@@ -72,7 +74,7 @@ def get_neighb_list(image, label):
                 feature = torch.cat((feature, dw), 0)
                 
                 if i+1 > shape_i-1:
-                  bc = bc
+                  bc = bc.to(device)
                   #print("shape bc:", bc, bc.shape)
                 else:
                   bc = torch.unsqueeze(image[i+1, j, k], 0)
