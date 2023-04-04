0965dfd61923282ba5b7595bb4b97af3cd63a266
Shivangi Nigam <shivi.nigam15@gmail.com>
2023-04-04 14:05:14 +0530

    trying to run again

--- models/get_patches.py
+++ models/get_patches.py
@@ -35,15 +35,15 @@ def get_patch_list(image, label, patch_size):
     size = patch_size
     #torch.Tensor.unfold(dimension, size, step)
     #slices the images into 8*8 size patches
-    patches = image.data.unfold(0, 3, 3).unfold(1, size, size).unfold(2, size, size)
+    patches = image.data.unfold(0, 3, 3).unfold(1, size, size).unfold(2, size, size).to(device)
     #print(patches.shape)
     count = 0
     total_patches_x = int(256/(patch_size))
     for i in range(total_patches_x):
         for j in range(total_patches_x):
             #print(patches[0][i][j].size)
-            resized_patch = resize_patch(patches[0][i][j], patch_size)
-            #print(resized_patch.shape)
+            resized_patch = resize_patch(patches[0][i][j], patch_size).to(device)
+            print(resized_patch.shape)
             features = torch.cat((features, resized_patch),0)
             labels = torch.cat((labels, label), 0)
             count = count + 1
