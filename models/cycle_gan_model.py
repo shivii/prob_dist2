f8a5a2411bfd5c02549ff04c8f6da029eaf6d05e
Shivangi Nigam <shivi.nigam15@gmail.com>
2023-04-05 22:46:32 +0530

    errror was in different devices of emb 2

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -204,14 +204,14 @@ class CycleGANModel(BaseModel):
 
         print("Computing embeddings") 
         tsne_embeddingsA = torch.cat((tsne_embeddingsA, featA.detach().cpu()), 0)
-        labels_A = torch.cat((labels_A, lblA.detach().cpu()),0)
+        labels_A = torch.cat((labels_A,  self.real),0)
         tsne_embeddingsA = torch.cat((tsne_embeddingsA, featCycleA.detach().cpu()), 0)
-        labels_A = torch.cat((labels_A, lblCycleA.detach().cpu()),0)
+        labels_A = torch.cat((labels_A, self.fake),0)
         
         tsne_embeddingsB = torch.cat((tsne_embeddingsB, featB.detach().cpu()), 0)
-        labels_B = torch.cat((labels_B, lblB.detach().cpu()),0)
+        labels_B = torch.cat((labels_B,  self.real),0)
         tsne_embeddingsB = torch.cat((tsne_embeddingsB, featCycleB.detach().cpu()), 0)
-        labels_B = torch.cat((labels_B, lblCycleB.detach().cpu()),0)
+        labels_B = torch.cat((labels_B, self.fake),0)
 
         tsne_embeddingsA = torch.cat((featA.detach().cpu(), featCycleA.detach().cpu()), 0)
         tsne_embeddingsB = torch.cat((featB.detach().cpu(), featCycleB.detach().cpu()), 0)
