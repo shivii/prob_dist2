c9d6810d2778959097de7d107c10bb96053974e4
Shivangi Nigam <shivi.nigam15@gmail.com>
2023-04-05 22:34:59 +0530

    errror was in embeddings size.. corrected cycle_gan_model to concatenate them

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -203,12 +203,15 @@ class CycleGANModel(BaseModel):
         featCycleB, lblCycleB = get_patches.get_patch_list(self.fake_B, self.fake, patch_size)
 
         print("Computing embeddings") 
-
-        tsne_embeddingsA = torch.cat((featA.detach().cpu(), featCycleA.detach().cpu()), 0)
-        tsne_embeddingsB = torch.cat((featB.detach().cpu(), featCycleB.detach().cpu()), 0)
-        labels_A = torch.cat((lblA, lblCycleA), 0)
-        labels_B = torch.cat((lblB, lblCycleB), 0)
+        tsne_embeddingsA = torch.cat((tsne_embeddingsA, featA.detach().cpu()), 0)
+        labels_A = torch.cat((labels_A, lblA),0)
+        tsne_embeddingsA = torch.cat((tsne_embeddingsA, featCycleA.detach().cpu()), 0)
+        labels_A = torch.cat((labels_A, lblCycleA),0)
         
+        tsne_embeddingsB = torch.cat((tsne_embeddingsB, featB.detach().cpu()), 0)
+        labels_B = torch.cat((labels_B, lblB),0)
+        tsne_embeddingsB = torch.cat((tsne_embeddingsB, featCycleB.detach().cpu()), 0)
+        labels_B = torch.cat((labels_B, lblCycleB),0) 
         
         print("Features shape:", featA.shape, featB.shape, featCycleA.shape, featCycleB.shape)
         print("embedings shape:", tsne_embeddingsA.shape, tsne_embeddingsB.shape)
