7929e199a95449770a9da1832382a4f789583090
Shivangi Nigam <shivi.nigam15@gmail.com>
2023-04-22 02:07:38 +0530

    changing the coeffecient

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -231,7 +231,7 @@ class CycleGANModel(BaseModel):
             self.loss_wt_hist_B = get_divergence(self.real_B, self.rec_B, pdf=3, klloss=opt.klloss)
             sum = sum + self.loss_wt_hist_A * coeff + self.loss_wt_hist_B * coeff
         if "4" in pdf_list:
-            coeff = 8e+04
+            coeff = 16e+05
             self.loss_img_pdf_A = get_divergence(self.real_A, self.rec_A, pdf=4, klloss=opt.klloss)
             self.loss_img_pdf_B = get_divergence(self.real_B, self.rec_B, pdf=4, klloss=opt.klloss)
             sum = sum + self.loss_img_pdf_A * coeff + self.loss_img_pdf_B * coeff
